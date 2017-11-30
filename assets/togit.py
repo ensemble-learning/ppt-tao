c7c09768206b9bca42ae8898e0939a945401ccee
Tao <chengtaoliterature@gmail.com>
2017-11-30 10:35:53 -0800

    bugfix

---
+++ assets/togit.py
@@ -0,0 +1,15 @@
+import os, sys
+if len(sys.argv) > 2:
+    cmd = "convert "
+    infile = sys.argv[1]
+    cmd += infile + " "
+    cmd += "-resize 800x400 " 
+    cmd += "-background none " 
+    cmd += "-compose Copy "
+    cmd += "-gravity center "
+    cmd += "-extent 800x400 "
+    #cmd += "-density 1200 "
+    outfile = sys.argv[2]
+    cmd += outfile
+    os.system(cmd)
+
