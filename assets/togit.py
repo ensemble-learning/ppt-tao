c775097160717952d83324483cf2f4e16c6a081f
Tao <chengtaoliterature@gmail.com>
2018-02-06 13:32:24 -0800

    bugfix

--- assets/togit.py
+++ assets/togit.py
@@ -5,7 +5,7 @@ if len(sys.argv) > 2:
     cmd += infile + " "
     cmd += "-resize 800x400 " 
     #cmd += "-background none " 
-    cmd += "-background 'rgb(0,0,0)'" 
+    cmd += "-background 'rgb(255,255,255)' " 
     cmd += "-compose Copy "
     cmd += "-gravity center "
     cmd += "-extent 800x400 "
@@ -13,5 +13,6 @@ if len(sys.argv) > 2:
     #cmd += "-density 1200 "
     outfile = sys.argv[2]
     cmd += outfile
+    print cmd
     os.system(cmd)
 
